a00ea8a75b32dd655487ff7d7a464bcdf9bfd91f
Daniil Temnyj <temnyy1@gmail.com>
2023-09-04 16:36:26 +0300

    Made modules standalone, create main

--- create_sfx.py
+++ create_sfx.py
@@ -1,41 +1,58 @@
 import os
 import sys
+import json
 
-def create_sfx(presentation_folder_path):
-    for file in os.listdir(presentation_folder_path):
+def create_sfx(settings, source_folder_path):
+    executable_name = ''
+    for file in os.listdir(source_folder_path):
         if file.endswith('.exe'):
-            presentation_name = file
+            executable_name = file
 
-    archive_name = os.path.split(presentation_folder_path)[-1]
+    archive_name = os.path.split(source_folder_path)[-1]
 
-    if os.path.exists(f'{presentation_folder_path}\\windeployqt'):
-        create_archive_command = f'7z a -t7z {archive_name}.7z -m0=lzma2 -mx=9 -aoa {presentation_folder_path}\\*.exe {presentation_folder_path}\\windeployqt\\* {presentation_folder_path}\\data {presentation_folder_path}\\..\\presentation_tool\\hotkeys.txt {presentation_folder_path}\\model.configuration'
+    create_archive_command = f'7z a -t7z {archive_name}.7z -m0=lzma2 -mx=9 -aoa'
+
+    if settings:
+        for item in settings["include_files"]:
+            create_archive_command += f' {source_folder_path}{item}'
+        if os.path.exists(f'{source_folder_path}\\windeployqt'):
+            create_archive_command += f' {source_folder_path}\\windeployqt\\*'
+        else:
+            for item in settings["additional_files"]:
+                create_archive_command += f' {source_folder_path}{item}'
     else:
-        create_archive_command = f'7z a -t7z {archive_name}.7z -m0=lzma2 -mx=9 -aoa {presentation_folder_path}\\*.exe {presentation_folder_path}\\styles {presentation_folder_path}\\platforms {presentation_folder_path}\\imageformats {presentation_folder_path}\\*.dll {presentation_folder_path}\\data {presentation_folder_path}\\..\\presentation_tool\\hotkeys.txt {presentation_folder_path}\\model.configuration'
+        create_archive_command += f' {source_folder_path}\\*'
 
-    print(create_archive_command)
 
-    config = f';!@Install@!UTF-8!\nRunProgram="{presentation_name}"\nGUIMode="2"\n;!@InstallEnd@!'
+    if executable_name:
+        config = f';!@Install@!UTF-8!\nInstallPath="{executable_name[:-4]}"\nRunProgram="{executable_name}"\nGUIMode="2"\n;!@InstallEnd@!'
+    else:
+        config = f';!@Install@!UTF-8!\nInstallPath="{archive_name}"\nGUIMode="2"\n;!@InstallEnd@!'
+        executable_name = f'{archive_name}.exe'
 
     for file in os.listdir(os.path.dirname(__file__)):
         if file.endswith('.sfx'):
             zip_module = file
 
-    create_sfx_command = f'COPY /b {zip_module} + config.txt + {archive_name}.7z {presentation_name}'
+    create_sfx_command = f'COPY /b {zip_module} + config.txt + {archive_name}.7z {executable_name}'
 
     with open(f'config.txt', 'w') as f:
         f.write(config)
 
+    print(create_sfx_command)
+
     os.system(create_archive_command)
+    print('---------------')
     os.system(create_sfx_command)
 
     os.remove(f'{archive_name}.7z')
 
 if __name__=='__main__':
     try:
-        presentation_folder_path = sys.argv[1]
-    except:
-        print(f'usage: create_sfx.py <input build folder>')
+        source_folder_path = sys.argv[1]
+    except IndexError:
+        print(f'Not Enough Arguments:')
+        print(f'\tusage: create_sfx.py <input source folder>')
         sys.exit(2)
 
-    create_sfx(presentation_folder_path)
+    create_sfx({}, source_folder_path)
--- upload_to_yandex_disk.py
+++ upload_to_yandex_disk.py
@@ -3,49 +3,96 @@ import yadisk
 import json
 import sys
 from datetime import date
-from create_sfx import create_sfx
 
 
-def upload_to_disk(token):
-    y = yadisk.YaDisk(token=token)
+def create_dir(y, path):
+    try:
+        y.mkdir(path)
+    except yadisk.exceptions.DirectoryExistsError:
+        pass
+
+def upload_file(y, src_path, target_path):
+    try:
+        y.upload(src_path, target_path)
+    except yadisk.exceptions.PathExistsError:
+        print(f'{target_path.split("/")[-1]} already exists, updating...')
+        y.remove(target_path)
+        y.upload(src_path, target_path)
+
+def upload_to_disk(settings, source_path='', target_path='', ignored=[]):
+    y = yadisk.YaDisk(token = settings['token'])
     if y.check_token():
         print('Token is correct, uploading')
     else:
         print("Token is incorrect, can't upload")
         sys.exit(3)
 
-    for file in os.listdir():
-        if file.endswith('.exe'):
-            presentation_name = file
+    if not target_path:
+        target_path = settings['default_folder']
 
     td = date.today().strftime("%d_%m_%Y")
 
-    folder_name = '/presentations/'
-    try:
-        y.mkdir(folder_name)
-    except yadisk.exceptions.DirectoryExistsError:
-        pass
+    total_path = '/'
+    for folder in target_path.split('/'):
+        if folder:
+            total_path += folder
+            create_dir(y, total_path)
+            total_path += '/'
 
-    folder_name += td
-    try:
-        y.mkdir(folder_name)
-    except yadisk.exceptions.DirectoryExistsError:
-        pass
+    if not source_path:
+        total_path += td
+        create_dir(y, total_path)
+        for file in os.listdir():
+            if file.endswith('.exe'):
+                executable_name = file
+                print(f'Uploading {executable_name}')
+                upload_file(y, executable_name, f"{total_path}/{executable_name}")
+                print('Upload complete')
+                if os.path.exists('old'):
+                    if not os.path.exists(os.path.join('old', td)):
+                        os.mkdir(os.path.join('old', td))
+                else:
+                    os.mkdir(os.path.join('old'))
+                    os.mkdir(os.path.join('old', td))
+                os.replace(os.path.join(executable_name), os.path.join('old', td, executable_name))
 
-    print(f'Uploading {presentation_name}')
-    y.upload(presentation_name, f"{folder_name}/{presentation_name}")
-    print('Upload complete')
+    else:
+        if os.path.isdir(source_path):
+            for adress, dirs, files in os.walk(source_path):
+                cur_path = total_path + adress.replace(source_path, '')[1:].replace('\\', '/') + ('/' if adress.replace(source_path, '')[1:] else '')
+                inters = set(ignored).intersection(cur_path.split('/'))
+                if inters:
+                    continue
+                for dir in dirs:
+                    if dir in ignored:
+                        continue
+                    print(f'Creating dir {dir} in {cur_path}')
+                    create_dir(y, f'{cur_path}{dir}')
+                for file in files:
+                    if file in ignored:
+                        continue
+                    print(f'Uploading {file} to {cur_path}')
+                    upload_file(y, os.path.join(adress, file), f'{cur_path}{file}')
+        else:
+            source_path = source_path.replace('\\', '/')
+            file = source_path.split("/")[-1]
+            print(f'Uploading {file} to {total_path}')
+            upload_file(y, source_path, f'{total_path}{file}')
 
 if __name__ == '__main__':
     with open('settings.json', encoding='utf-8') as json_file:
         settings = json.load(json_file)
     
     try:
-        presentation_folder_path = sys.argv[1]
-    except:
-        print(f'usage: upload_to_yandex_disk.py <input build folder>')
+        source_folder_path = sys.argv[1]
+        target_folder_path = sys.argv[2]
+    except IndexError:
+        print(f'Not Enough Arguments:')
+        print(f'\tusage: upload_to_yandex_disk.py <source folder or path to file to upload> <yandex disk destination folder>')
         sys.exit(2)
 
-    create_sfx(presentation_folder_path)
+    ignored = ['.git', '.gitignore', '.env', 'old', '__pycache__', 'config.txt']
 
-    upload_to_disk(settings["token"])
+    upload_to_disk(settings, source_folder_path, target_folder_path.replace('\\', '/'), ignored)
+
+    print('Complete')
